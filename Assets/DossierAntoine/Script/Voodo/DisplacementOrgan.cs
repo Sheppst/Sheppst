0da6acb42fa85e972c6b231a3d5ea543a013b543
Antoine Yon <antoine.yon01@gmail.com>
2024-09-25 21:56:47 +0200

    up

--- Assets/DossierAntoine/Script/Voodo/DisplacementOrgan.cs
+++ Assets/DossierAntoine/Script/Voodo/DisplacementOrgan.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class DisplacementOrgan : MonoBehaviour
 {
-    [SerializeField] private List<Transform> Organs;
+    [SerializeField] private List<GameObject> Organs;
+    [SerializeField] private List<Transform> Emplacement;
     [SerializeField] private Transform Mid;
+    [SerializeField] private float Distance;
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < Organs.Count; i++)
+        {
+            if (Mid.position.z > Emplacement[i].position.z)
+                continue;
+            GameObject InstOrg = Instantiate(Organs[i], (Emplacement[i].position - Mid.position).normalized * Distance, Quaternion.identity);
+            InstOrg.transform.LookAt(Mid.position);
+        }
+        //RotateDollStart();
+        for (int i = 0; i < Organs.Count; i++)
+        {
+            if (Mid.position.z < Emplacement[i].position.z)
+                continue;
+            GameObject InstOrg = Instantiate(Organs[i], (Emplacement[i].position - Mid.position).normalized * Distance, Quaternion.identity);
+            InstOrg.transform.LookAt(Mid.position);
+        }
+        enabled = false;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        enabled = false;
     }
 
     // Update is called once per frame
